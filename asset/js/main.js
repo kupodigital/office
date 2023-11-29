dc7bd8ac3f27b962d742a4961783d6e27850b12b
Weslley Alves <weslley@wezo.com.br>
2023-11-29 02:52:44 -0300

    update interface

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,12 +1,14 @@
-const { app, BrowserWindow, ipcMain, dialog, nativeImage } = require('electron');
+const { app, BrowserWindow, ipcMain, dialog, nativeImage, Menu, MenuItem, shell } = require('electron');
 const path = require('path');
 const fs = require('fs');
+const Jimp = require("jimp");
 
 let mainWindow;
 
 
 
 const folderPath = path.join(__dirname + "../../../", '_operations', 'original');
+const folderPathToJpeg = path.join(__dirname + "../../../", '_operations', 'toFormatJpeg');
 const iconPath = path.join(__dirname + "../../../", 'asset', 'img');
 
 
@@ -14,17 +16,34 @@ const iconPath = path.join(__dirname + "../../../", 'asset', 'img');
 
 function createWindow() {
 
-    //const appIcon = new Tray(iconPath);
+    const menu = new Menu();
+
+    menu.append(new MenuItem({
+        label: 'Kupo Office',
+        submenu: [{
+            role: 'Donate',
+            label: 'Donate Now',
+            accelerator: process.platform === 'darwin' ? 'Alt+Cmd+I' : 'Alt+Shift+I',
+            click: () => {
+                console.log('Kupo Office rocks!')
+            }
+        }]
+    }))
+
+    Menu.setApplicationMenu(menu);
 
     mainWindow = new BrowserWindow({
         autoHideMenuBar: true,
         icon: nativeImage.createFromPath(path.join(iconPath, 'logo.png')),
-        backgroundColor: '#2e2c29',
+        backgroundColor: '#000000',
         center: true,
         transparent: false,
-        width: 1080,
+        width: 1260,
         height: 750,
+        minWidth: 860,
         frame: true,
+        modal: true,
+        show: true,
         thickFrame: false,
         darkTheme: true,
         hasShadow: false,
@@ -50,6 +69,36 @@ function createWindow() {
         mainWindow = null;
     });
 
+    ipcMain.on('open-file-dialog-for-file', async function (event) {
+        try {
+            const result = await dialog.showOpenDialog(mainWindow, {
+                filters: [{ name: 'Images', extensions: ['jpg', 'jpeg', 'tiff', 'png', 'gif'] }],
+                properties: ['openFile', 'multiSelections']
+            });
+
+            if (!result.canceled && result.filePaths.length > 0) {
+
+                if (!fs.existsSync(folderPath)) {
+                    fs.mkdirSync(folderPath, { recursive: true });
+                }
+
+                let files = [];
+                for (const filePath of result.filePaths) {
+                    const fileName = path.basename(filePath);
+                    files.push(fileName);
+
+                    const destinationPath = path.join(folderPath, fileName);
+                    fs.copyFileSync(filePath, destinationPath);
+                }
+
+                event.sender.send('selected-files', result.filePaths);
+                event.sender.send('refresh-list', files);
+            }
+        } catch (err) {
+            console.error('Error opening file dialog:', err);
+        }
+    });
+    /*
     ipcMain.on('open-file-dialog-for-file', function (event) {
         dialog.showOpenDialog(mainWindow, {
             filters: [{ name: 'Images', extensions: ['jpg', 'jpeg', 'tiff', 'png', 'gif'] }],
@@ -61,18 +110,25 @@ function createWindow() {
                     fs.mkdirSync(folderPath, { recursive: true });
                 }
 
+                let files = [];
                 result.filePaths.forEach(filePath => {
+
                     const fileName = path.basename(filePath);
+                    files.push(fileName);
+
                     const destinationPath = path.join(folderPath, fileName);
                     fs.copyFileSync(filePath, destinationPath);
+
                 });
                 event.sender.send('selected-files', result.filePaths);
-
+                event.sender.send('refresh-list', files);
             }
         }).catch(err => {
             console.error('Error opening file dialog:', err);
         });
     });
+    */
+
 
     ipcMain.on('file-list', (event, fileList) => {
         event.sender.send('list-files');
@@ -95,6 +151,54 @@ function createWindow() {
         }
         event.sender.send('update-file-list');
     });
+
+
+    ipcMain.on('preview-file', async (event, fileName, w, h) => {
+        const filePath = folderPath + '/' + fileName;
+        if (fs.existsSync(filePath)) {
+            const previewWindow = new BrowserWindow({
+                width: w,
+                height: h,
+                center: true,
+                title: 'Preview',
+                transparent: true,
+                frame: true,
+                modal: true,
+                autoHideMenuBar: true,
+                webPreferences: {
+                    nodeIntegration: true
+                }
+            });
+            previewWindow.loadURL(`file://${filePath}`);
+        }
+    });
+
+    ipcMain.on('open-external-link', (event, url) => {
+        shell.openExternal(url);
+    });
+
+    async function resizeImage(imagePath, w = Jimp.AUTO, h = 1000) {
+        const newImageName = imagePath.replace('/original/', '/resized/');
+        Jimp.read(imagePath)
+            .then((image) => {
+                return image.resize(w, h).writeAsync(newImageName); // save
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    }
+
+    async function greyscaleImage(imagePath) {
+        // https://www.npmjs.com/package/jimp
+        const newImageName = imagePath.replace('/original/', '/resized/');
+        Jimp.read(imagePath)
+            .then((image) => {
+                return image.greyscale().writeAsync(newImageName); // save
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    }
 
     function listFilesInFolder(folderPath) {
         return new Promise((resolve, reject) => {
--- asset/js/renderer.js
+++ asset/js/renderer.js
@@ -1,19 +1,35 @@
-const { ipcRenderer } = require('electron');
+const { ipcRenderer, BrowserWindow } = require('electron');
 const fs = require('fs');
 const mime = require('mime');
 const path = require('path');
 const sharp = require("sharp");
 
-const buttonCreated = document.getElementById('upload');
-
 const folderPath = path.join(__dirname + '../../', '_operations', 'original');
 
+const buttonCreated = document.getElementById('upload');
 buttonCreated.addEventListener('click', function () {
     ipcRenderer.send('open-file-dialog-for-file');
 });
 
+
+const externalLinks = document.querySelectorAll('.external-link');
+
+externalLinks.forEach(link => {
+    link.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        // Obtém o valor do href do link clicado
+        const externalUrl = this.href;
+
+        // Envia uma mensagem para o processo principal para abrir o link externo
+        ipcRenderer.send('open-external-link', externalUrl);
+    });
+});
+
+
+
 async function updateFileList(fileList) {
-    console.group('loop file');
+
     const fileListElement = document.getElementById('fileList');
     fileListElement.innerHTML = '';
     for (const fileName of fileList) {
@@ -40,10 +56,11 @@ async function updateFileList(fileList) {
             const hasAlphaCell = document.createElement('td');
             const actionsCell = document.createElement('td');
             const deleteButton = document.createElement('button');
+            const previewButton = document.createElement('button');
 
             row.className = "pb-4 hover:bg-lime-500 hover:text-black";
             row.setAttribute('title', fileName);
-            previewCell.className = "align-middle hover:bg-lime-500 hover:text-black rounded-l-full";
+            previewCell.className = "align-middle hover:bg-lime-500 rounded-l-xl";
             nameCell.className = "w-28 align-middle hover:bg-lime-500 hover:text-black";
             sizeCell.className = "align-middle hover:bg-lime-500 hover:text-black";
             formatCell.className = "align-middle hover:bg-lime-500 hover:text-black";
@@ -51,7 +68,12 @@ async function updateFileList(fileList) {
             densityCell.className = "align-middle hover:bg-lime-500 hover:text-black";
             hasAlphaCell.className = "align-middle hover:bg-lime-500 hover:text-black";
             actionsCell.className = "align-middle hover:bg-lime-500 hover:text-black";
-            deleteButton.className = "align-middle hover:bg-lime-500 hover:text-black";
+            deleteButton.className = "align-middle hover:bg-lime-500 hover:text-black ml-3 inline";
+            previewButton.className = "align-middle hover:bg-lime-500 hover:text-black ml-3 inline";
+
+
+            let width = 800;
+            let height = 600;
 
             sharp(pathFile)
                 .metadata()
@@ -60,9 +82,11 @@ async function updateFileList(fileList) {
                     dimensionCell.innerHTML = `${metadata.width} x ${metadata.height}`;
                     densityCell.innerHTML = `${metadata.density}`;
                     hasAlphaCell.innerHTML = `${metadata.hasAlpha}`;
+                    width = metadata.width;
+                    height = metadata.height;
                 })
                 .catch((err) => {
-                    console.error("52", err);
+                    console.error("72", err);
                 });
 
             // size file
@@ -71,17 +95,18 @@ async function updateFileList(fileList) {
             sizeCell.innerHTML = `<span>${sizefile}</span>`;
 
             // image preview
-            previewCell.innerHTML = `<img src="${pathFile}" class="w-full h-full rounded-xl border-2 border-lime-500 bg-lime-500 " />`
+            previewCell.innerHTML = `<img src="${pathFile}" class="w-full h-full rounded-xl border-2 hover:border-r-0 border-lime-500 bg-lime-500 " />`
 
 
             nameCell.innerHTML = `<span class="truncate w-80 grid text-md px-3" >${fileName}</span>`;
 
-
-            deleteButton.innerHTML = `<svg title="Delete File" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6  hover:text-black cursor-pointer"> <path stroke-linecap="round" stroke-linejoin="round" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" /> </svg>`;
+            deleteButton.innerHTML = `<svg class="w-6 h-6 hover:text-black cursor-pointer" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0.7" d="M1 5h16M7 8v8m4-8v8M7 1h4a1 1 0 0 1 1 1v3H6V2a1 1 0 0 1 1-1ZM3 5h12v13a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V5Z"/></svg>`;
             deleteButton.onclick = () => deleteFile(fileName);
 
-            // Adicionar células à linha
+            previewButton.innerHTML = `<svg class="w-6 h-6 hover:text-black cursor-pointer" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 14"> <g stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0.7"> <path d="M10 10a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/> <path d="M10 13c4.97 0 9-2.686 9-6s-4.03-6-9-6-9 2.686-9 6 4.03 6 9 6Z"/>  </g>  </svg>`;
+            previewButton.onclick = () => previewFile(fileName, width, height);
 
+            // Adicionar células à linha
             row.appendChild(previewCell);
             row.appendChild(nameCell);
             row.appendChild(sizeCell);
@@ -89,33 +114,47 @@ async function updateFileList(fileList) {
             row.appendChild(formatCell);
             row.appendChild(actionsCell);
             actionsCell.appendChild(deleteButton);
+            actionsCell.appendChild(previewButton);
 
             // Adicionar linha à tabela
             fileListElement.appendChild(row);
+
         } catch (error) {
             console.error('Erro ao ler o arquivo:', error);
         }
     }
-
-    console.groupEnd();
 }
 
-// Função para deletar um arquivo
-function deleteFile(fileName) {
-
+function previewFile(fileName, w, h) {
     const filePath = path.join(folderPath, fileName);
-
     if (fs.existsSync(filePath)) {
-        console.error('deleteFile rendered', filePath);
+        ipcRenderer.send('preview-file', fileName, w / 1000, h / 1000);
+    }
+}
+
+function deleteFile(fileName) {
+    const filePath = path.join(folderPath, fileName);
+    if (fs.existsSync(filePath)) {
         ipcRenderer.send('delete-file', fileName);
         ipcRenderer.send('list-files'); // update
-
     } else {
         console.error('File not found:', filePath);
     }
 }
 
-// Quando a página é carregada, solicite a lista de arquivos ao processo principal
+function listFilesInFolder(folderPath) {
+    return new Promise((resolve, reject) => {
+        fs.readdir(folderPath, (err, files) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(files);
+            }
+        });
+    });
+}
+
+
 document.addEventListener('DOMContentLoaded', () => {
 
     ipcRenderer.send('list-files');
@@ -124,9 +163,18 @@ document.addEventListener('DOMContentLoaded', () => {
         updateFileList(fileList);
     });
 
+    ipcRenderer.on('refresh-list', async (event, fileList) => {
+        try {
+            const fileList = await listFilesInFolder(folderPath);
+
+            updateFileList(fileList);
+        } catch (error) {
+            console.error('Error listing files:', error);
+        }
+    });
+
     ipcRenderer.on('file-list-error', (event, errorMessage) => {
         console.error('File list error:', errorMessage);
-        // Adicione lógica adicional se necessário
     });
 
 });
